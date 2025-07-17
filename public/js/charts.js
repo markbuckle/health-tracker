fa3fdca73c0c5a10076e2c19d3e58ea4e39f3da0
Mark Buckle <markbuckle92@gmail.com>
2025-07-17 10:26:44 -0230

    Updated plotly chart x-axis

--- public/js/charts.js
+++ public/js/charts.js
@@ -188,6 +188,182 @@ function forcePlotlyRender() {
     });
 }
 
+// x axis helper function
+function getOptimalXAxisConfig(dates, containerWidth) {
+    if (!dates || dates.length === 0) {
+        return {
+            type: 'date',
+            showgrid: false,
+            zeroline: false,
+            tickangle: 0,
+            tickformat: '%b',
+            tickfont: {
+                family: 'Arial, sans-serif',
+                size: containerWidth < 350 ? 10 : 12,
+                color: '#666'
+            }
+        };
+    }
+
+    // Sort dates to get proper range
+    const sortedDates = [...dates].sort((a, b) => a - b);
+    const earliestDate = sortedDates[0];
+    const latestDate = sortedDates[sortedDates.length - 1];
+    
+    // Calculate time span in days
+    const timeSpanDays = (latestDate - earliestDate) / (1000 * 60 * 60 * 24);
+    
+    let tickConfig = {
+        type: 'date',
+        showgrid: false,
+        zeroline: false,
+        tickangle: 0, // Always horizontal
+        tickfont: {
+            family: 'Arial, sans-serif',
+            size: containerWidth < 350 ? 10 : 12,
+            color: '#666'
+        },
+        showspikes: true,
+        spikethickness: 1,
+        spikedash: 'dot',
+        spikecolor: '#999',
+        spikemode: 'across'
+    };
+
+    // Single data point - show months around it
+    if (dates.length === 1) {
+        const centerDate = dates[0];
+        const tickVals = [];
+        
+        // Create 5 monthly ticks centered around the single date
+        for (let i = -2; i <= 2; i++) {
+            const tickDate = new Date(centerDate);
+            tickDate.setMonth(tickDate.getMonth() + i);
+            tickDate.setDate(15); // Middle of month for consistent positioning
+            tickVals.push(tickDate);
+        }
+        
+        tickConfig.tickmode = 'array';
+        tickConfig.tickvals = tickVals;
+        tickConfig.tickformat = '%b';
+        
+        // Set range to show the full 5-month span
+        const rangeStart = new Date(centerDate);
+        rangeStart.setMonth(rangeStart.getMonth() - 3);
+        const rangeEnd = new Date(centerDate);
+        rangeEnd.setMonth(rangeEnd.getMonth() + 3);
+        
+        tickConfig.range = [rangeStart, rangeEnd];
+        
+        return tickConfig;
+    }
+
+    // Multiple data points - determine format based on time span
+    if (timeSpanDays <= 365) {
+        // Less than a year - show months
+        tickConfig.tickformat = '%b';
+        
+        // Get unique months from the data
+        const uniqueMonths = new Set();
+        dates.forEach(date => {
+            const monthKey = `${date.getFullYear()}-${date.getMonth()}`;
+            uniqueMonths.add(monthKey);
+        });
+        
+        // If we have few unique months, add padding months
+        if (uniqueMonths.size <= 4) {
+            const monthTickVals = [];
+            
+            // Add months before earliest date
+            for (let i = 2; i >= 1; i--) {
+                const beforeDate = new Date(earliestDate);
+                beforeDate.setMonth(beforeDate.getMonth() - i);
+                beforeDate.setDate(15);
+                monthTickVals.push(beforeDate);
+            }
+            
+            // Add months for each unique month in data
+            dates.forEach(date => {
+                const monthDate = new Date(date.getFullYear(), date.getMonth(), 15);
+                if (!monthTickVals.some(tick => 
+                    tick.getFullYear() === monthDate.getFullYear() && 
+                    tick.getMonth() === monthDate.getMonth()
+                )) {
+                    monthTickVals.push(monthDate);
+                }
+            });
+            
+            // Add months after latest date
+            for (let i = 1; i <= 2; i++) {
+                const afterDate = new Date(latestDate);
+                afterDate.setMonth(afterDate.getMonth() + i);
+                afterDate.setDate(15);
+                monthTickVals.push(afterDate);
+            }
+            
+            // Sort and use as tick values
+            monthTickVals.sort((a, b) => a - b);
+            tickConfig.tickmode = 'array';
+            tickConfig.tickvals = monthTickVals;
+            
+            // Set range to accommodate all ticks
+            const rangeStart = new Date(monthTickVals[0]);
+            rangeStart.setDate(1);
+            const rangeEnd = new Date(monthTickVals[monthTickVals.length - 1]);
+            rangeEnd.setMonth(rangeEnd.getMonth() + 1);
+            rangeEnd.setDate(0); // Last day of previous month
+            
+            tickConfig.range = [rangeStart, rangeEnd];
+        } else {
+            // Many months - let Plotly auto-generate with some padding
+            const rangeStart = new Date(earliestDate);
+            rangeStart.setDate(rangeStart.getDate() - 30);
+            const rangeEnd = new Date(latestDate);
+            rangeEnd.setDate(rangeEnd.getDate() + 30);
+            
+            tickConfig.range = [rangeStart, rangeEnd];
+        }
+        
+    } else if (timeSpanDays <= 1095) {
+        // 1-3 years - show months only, years will be added as annotations
+        tickConfig.tickformat = '%b';
+        tickConfig.dtick = 'M3'; // Every 3 months
+        
+        const rangeStart = new Date(earliestDate);
+        rangeStart.setMonth(rangeStart.getMonth() - 3);
+        const rangeEnd = new Date(latestDate);
+        rangeEnd.setMonth(rangeEnd.getMonth() + 3);
+        
+        tickConfig.range = [rangeStart, rangeEnd];
+        
+    } else if (timeSpanDays <= 3650) {
+        // 3-10 years - show years
+        tickConfig.tickformat = '%Y';
+        tickConfig.dtick = 'M12'; // Every 12 months (yearly)
+        
+        const rangeStart = new Date(earliestDate);
+        rangeStart.setFullYear(rangeStart.getFullYear() - 1);
+        const rangeEnd = new Date(latestDate);
+        rangeEnd.setFullYear(rangeEnd.getFullYear() + 1);
+        
+        tickConfig.range = [rangeStart, rangeEnd];
+        
+    } else {
+        // More than 10 years - show every few years
+        tickConfig.tickformat = '%Y';
+        tickConfig.dtick = 'M24'; // Every 2 years
+        
+        const rangeStart = new Date(earliestDate);
+        rangeStart.setFullYear(rangeStart.getFullYear() - 2);
+        const rangeEnd = new Date(latestDate);
+        rangeEnd.setFullYear(rangeEnd.getFullYear() + 2);
+        
+        tickConfig.range = [rangeStart, rangeEnd];
+    }
+
+    return tickConfig;
+}
+
 // Core chart rendering function (optimized for performance)
 function createBiomarkerChart(biomarkerElement, biomarkerName) {
     if (!biomarkerElement) {
@@ -456,74 +632,7 @@ function createBiomarkerChart(biomarkerElement, biomarkerName) {
         hoverdistance: 100,
         hovermode: 'x unified',
         spikedistance: -1,
-        xaxis: {
-            type: 'date',
-            showgrid: false,
-            zeroline: false,
-            tickformat: '%b', // Only show month abbreviation
-            tickfont: {
-                family: 'Arial, sans-serif',
-                size: containerWidth < 350 ? 10 : 12,
-                color: '#666'
-            },
-            range: [
-                // Expand range to show at least 5 months
-                new Date(Math.min(...dates) - 60 * 24 * 60 * 60 * 1000),  // 60 days before earliest date
-                new Date(Math.max(...dates) + 60 * 24 * 60 * 60 * 1000)   // 60 days after latest date
-            ],
-            showspikes: true,
-            spikethickness: 1,
-            spikedash: 'dot',
-            spikecolor: '#999',
-            spikemode: 'across',
-            // Ensure we show multiple months if they exist
-            tickmode: 'array',
-            tickvals: (function() {
-                // If we have few data points, create 5 month ticks centered around data
-                if (dates.length <= 3) {
-                    const centerDate = dates.length === 0 ? new Date() : 
-                                       new Date((Math.min(...dates.map(d => d.getTime())) + 
-                                                Math.max(...dates.map(d => d.getTime()))) / 2);
-                    
-                    // Create array of 5 months centered on data
-                    return [-2, -1, 0, 1, 2].map(monthOffset => {
-                        const date = new Date(centerDate);
-                        date.setMonth(date.getMonth() + monthOffset);
-                        return new Date(date.getFullYear(), date.getMonth(), 15);
-                    });
-                } else {
-                    // If we have more data points, use those with a bit of padding
-                    const uniqueMonths = Array.from(new Set(dates.map(d => 
-                        new Date(d.getFullYear(), d.getMonth(), 15).getTime()
-                    ))).map(timestamp => new Date(timestamp));
-                    
-                    // Sort dates chronologically
-                    uniqueMonths.sort((a, b) => a - b);
-                    
-                    // If we have less than 5 unique months, add months on either side
-                    if (uniqueMonths.length < 5) {
-                        const padding = Math.ceil((5 - uniqueMonths.length) / 2);
-                        
-                        // Add months before
-                        for (let i = 1; i <= padding; i++) {
-                            const firstDate = new Date(uniqueMonths[0]);
-                            firstDate.setMonth(firstDate.getMonth() - i);
-                            uniqueMonths.unshift(firstDate);
-                        }
-                        
-                        // Add months after
-                        for (let i = 1; i <= padding; i++) {
-                            const lastDate = new Date(uniqueMonths[uniqueMonths.length - 1]);
-                            lastDate.setMonth(lastDate.getMonth() + i);
-                            uniqueMonths.push(lastDate);
-                        }
-                    }
-                    
-                    return uniqueMonths;
-                }
-            })(),
-            showticklabels: true
-        },
+        xaxis: getOptimalXAxisConfig(dates, containerWidth),
         yaxis: {
             showgrid: true,
             gridcolor: '#e5e5e5',
